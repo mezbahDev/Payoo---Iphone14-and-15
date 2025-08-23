9c90efb2d01f95742746d8f83e691c2f39cde587
mezbahDev <mezbahuddin9029@gmail.com>
2025-08-23 13:48:11 +0600

    login page styled successfully

---
+++ script.js
@@ -0,0 +1,27 @@
+// login button function
+
+const loginBtn = document.getElementById("loginBtn");
+
+const phoneNumber = 1521559029;
+const password = 1234;
+
+loginBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const mobileNumberValue = document.getElementById("mobileNumber").value;
+  const pinValue = document.getElementById("pin").value;
+
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+  const pinValueConverted = parseInt(pinValue);
+
+  console.log(mobileNumberValueConverted);
+
+  if (
+    mobileNumberValueConverted === phoneNumber &&
+    pinValueConverted === password
+  ) {
+    window.location.href = "home.html";
+  } else {
+    alert("Mobile Number and Password not maching");
+  }
+});
